155566a4c121b6b954167d344ce9f23ab2d1768d
Ogihara Ryo <r-ogihara@froide-kk.co.jp>
2016-05-31 21:24:51 +0900

    ユーザー認証システム実装 (#7)

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :users
+
+  get 'signin' => 'sessions#new'
+  post 'signin' => 'sessions#create'
+  delete 'signout' => 'sessions#destroy'
 end
