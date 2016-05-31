0da2f131996e92267de4750382791d2d31d97455
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-05-31 20:47:29 +0900

    User の CRUD 仮実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  before_create :create_remember_token
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
