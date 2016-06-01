0d4a0d428c44427c304698148adf9331f0143b27
kokoriru <kokorirury@yahoo.co.jp>
2016-06-02 02:02:05 +0900

    comment ページに日本語を適応

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,7 +29,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to report_path(@comment.report_id), notice: 'Comment was successfully created.' }
+        format.html { redirect_to report_path(@comment.report_id), notice: 'コメントは正常に作成されました' }
         format.json { render :show, status: :created, location: report_path(@comment.report_id) }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to @comment, notice: 'コメントは正常に更新されました' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to comments_url, notice: 'コメントは正常に削除されました' }
       format.json { head :no_content }
     end
   end
