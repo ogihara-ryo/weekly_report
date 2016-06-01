7c29ba6e0d87c8dc7c0a09b49489c8c7851902ee
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-06-01 20:26:53 +0900

    seed ファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Comment.delete_all
+Comment.connection.execute(%|SELECT setval ('comments_id_seq', 1, false)|)
+Report.delete_all
+Report.connection.execute(%|SELECT setval ('reports_id_seq', 1, false)|)
+User.delete_all
+User.connection.execute(%|SELECT setval ('users_id_seq', 1, false)|)
+
+1.upto(10) do |i|
+  user = User.create(
+    signin_id: "user#{i}",
+    password: 'password',
+    password_confirmation: 'password'
+  )
+
+  report = Report.create(
+    user: user,
+    place: "user#{user.id}-place",
+    overwork: user.id,
+    work: "user#{user.id}-work",
+    probrem: "user#{user.id}-probrem",
+    solution: "user#{user.id}-solution",
+    plan: "user#{user.id}-plan",
+    other: "user#{user.id}-other"
+  )
+
+  Comment.create(
+    user: user,
+    report: report,
+    description: "report#{report.id}-comment"
+  )
+end
