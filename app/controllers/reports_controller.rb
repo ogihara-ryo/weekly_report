1ba19cc873174b4876a70dc62e94286bd82c83cf
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-06-03 21:51:15 +0900

    json 系リクエストに関する記述を削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,75 +1,51 @@
 class ReportsController < ApplicationController
-  before_action :set_report, only: [:show, :edit, :update, :destroy]
+  before_action :set_report, only: %i(show edit update destroy)
 
-  # GET /reports
-  # GET /reports.json
   def index
     @reports = Report.all.order(created_at: :desc)
   end
 
-  # GET /reports/1
-  # GET /reports/1.json
   def show
     @comment = Comment.new
   end
 
-  # GET /reports/new
   def new
     @report = Report.new
   end
 
-  # GET /reports/1/edit
   def edit
   end
 
-  # POST /reports
-  # POST /reports.json
   def create
     @report = Report.new(report_params)
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to @report, notice: '作成しました' }
-        format.json { render :show, status: :created, location: @report }
-      else
-        format.html { render :new }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to @report, notice: '作成しました'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /reports/1
-  # PATCH/PUT /reports/1.json
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: '更新しました' }
-        format.json { render :show, status: :ok, location: @report }
-      else
-        format.html { render :edit }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.update(report_params)
+      redirect_to @report, notice: '更新しました'
+    else
+      render :edit
     end
   end
 
-  # DELETE /reports/1
-  # DELETE /reports/1.json
   def destroy
     @report.destroy
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: '削除しました' }
-      format.json { head :no_content }
-    end
+    redirect_to reports_url, notice: '削除しました'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def report_params
-      params.require(:report).permit(:user_id, :place, :overwork, :work, :probrem, :solution, :plan, :other)
-    end
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:user_id, :place, :overwork, :work, :probrem, :solution, :plan, :other)
+  end
 end
