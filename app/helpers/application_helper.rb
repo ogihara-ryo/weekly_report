39a3b69a9f9c87268565ff839b2afa825cb13bb8
Ogihara Ryo <r-ogihara@froide-kk.co.jp>
2016-06-03 22:32:50 +0900

    Refactoring (#38)
    * ルーティング記述順変更
    
    * current_user メソッドを SessionsHelper に移動
    
    * 不要ファイルを削除
    
    * json 系リクエストに関する記述を削除
    
    * Comment の index, show, new, edit アクションを削除
    
    * 不要なスタイルシートを削除
    
    * 不要な改行を削除
    
    * page_title メソッドの条件演算子化
    
    * 1週間分の日付取得メソッドリファクタリング
    
    * サインインフォームの ID フィールドに自動フォーカスする
    
    * View の表記揺らぎを調整
    
    * 日時フォーマットの i18n 対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,14 @@
 module ApplicationHelper
-  def current_user
-    remember_token = User.encrypt(cookies[:remember_token])
-    @user ||= User.find_by(remember_token: remember_token)
-  end
-
-  def current_user?(user)
-    user == current_user
-  end
-
   def name_with_comment_count(report)
     report.comments.present? ? "#{report.user.name} (#{report.comments.count})" : report.user.name
   end
 
   def page_title(title)
-    if title.blank?
-      WeeklyReport::Application.config.title
-    else
-      "#{title} | #{WeeklyReport::Application.config.title}"
-    end
+    title.blank? ? WeeklyReport::Application.config.title : "#{title} | #{WeeklyReport::Application.config.title}"
   end
 
-  def weeks_ago(value)
-    from = (Date.today - value.week).beginning_of_week
+  def all_day_of_week_ago(value)
+    from = (Date.today - value.week).beginning_of_week.beginning_of_day
     to = (from + 6.day).end_of_day
     from..to
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,4 +14,13 @@ module SessionsHelper
     session[:user_id] = nil
     cookies.delete(:remember_token)
   end
+
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    @user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
 end
