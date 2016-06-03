3e1a71acecec3c9e3c261f3ba4b52f3ec641a1c3
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-06-03 21:44:15 +0900

    current_user メソッドを SessionsHelper に移動

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def current_user
-    remember_token = User.encrypt(cookies[:remember_token])
-    @user ||= User.find_by(remember_token: remember_token)
-  end
-
-  def current_user?(user)
-    user == current_user
-  end
-
   def name_with_comment_count(report)
     report.comments.present? ? "#{report.user.name} (#{report.comments.count})" : report.user.name
   end
